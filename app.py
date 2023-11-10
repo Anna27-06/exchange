5add73328e931f3b6cea102b09714247f7e5e305
Anna27-06 <144484940+Anna27-06@users.noreply.github.com>
2023-11-10 22:25:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import telebot
 
 from config import keys, TOKEN
-from utils import ConvertionException, CryptoConverter
+from extensions import ConvertionException, CryptoConverter
 
 bot = telebot.TeleBot(TOKEN)
 # @bot.message_handler()
